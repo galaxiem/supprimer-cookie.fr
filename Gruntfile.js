e7aab666ce599f8d01e57bd396f90fbccd6c7f06
Nicolas Peltier <peltier.nicolas@gmail.com>
2014-12-09 23:28:25 +0100

    adding ftp deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,13 +24,27 @@ module.exports = function (grunt) {
 			  "www/styles/style.css": "less/style.less"
 			}
 		  }
-		}
-		
-    });
+		},
+		'ftp-deploy': {
+		  build: {
+			auth: {
+			  host: 'ftp.cluster011.ovh.net',
+			  port: 21,
+			  authPath : process.env.GM_PATH,
+			  authKey: 'cookie'
+			},
+			src: './www',
+			dest: './www',
+			"exclusions":['./www/**/.DS_Store']
+		  }
+    	}
+	});
 
     grunt.loadNpmTasks('grunt-jade');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
 
     // Default task(s).
     grunt.registerTask('default', ['jade','less']);
+	grunt.registerTask('deploy', ['ftp-deploy']);
 };
